599b5f60296e754015446d9221eb8d675c5487bf
PKT <rudxorrmrwkd@gmail.com>
2025-04-14 14:32:17 +0900

    TextRPG 시작

---
+++ ConsoleApp1/ConsoleApp1/MainGame.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Data;
+
+namespace TextRPG
+{
+  
+
+    class Program
+    {
+      
+       
+     
+
+
+        static void Main(string[] args)
+        {
+            //여기가 이제 실행 단계 
+            Console.WriteLine("TEXT RPG게임 세계에 오신걸 환영합니다.");
+
+           // Game game = new Game();
+           // game.Update(); 이렇게 사용가능 
+        }
+    }
+
+    class Game
+    {
+        void Start()
+        { }
+        public void Update()
+        {
+            while (true)
+            {
+                Console.WriteLine("에러에러에");
+            }
+        }
+    }
+}
+
